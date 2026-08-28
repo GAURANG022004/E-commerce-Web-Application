8425f487e8fc7313a30916a4ddc39e25ec3498a8
Gaurang <gaurangmali11@gmail.com>
2026-08-28 23:24:32 +0530

    Improve imports in CartController

--- src/main/java/com/Bigproject/ecommerce_springboot/Controller/CartController.java
+++ src/main/java/com/Bigproject/ecommerce_springboot/Controller/CartController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.Bigproject.ecommerce_springboot.productnotfoundexception;
 import com.Bigproject.ecommerce_springboot.Repository.ProductRepository;
