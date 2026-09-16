4e54420b50b766e28fdead6af228229f13630e37
Gaurang <gaurangmali11@gmail.com>
2026-09-16 23:41:02 +0530

    Fix formatting of email field in User entity

--- src/main/java/com/Bigproject/ecommerce_springboot/entity/User.java
+++ src/main/java/com/Bigproject/ecommerce_springboot/entity/User.java
@@ -18,7 +18,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long user_id;
 	private String username;
-	private String email;
+	private String email;	
 	private String userpassword;
 	private String role;
 	private String status;
