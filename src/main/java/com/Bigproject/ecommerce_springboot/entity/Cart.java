a06bdd8f059e8cb3670408f039b33615159b606f
Gaurang <gaurangmali11@gmail.com>
2026-09-16 23:40:40 +0530

    Remove unnecessary @Component annotation from Cart entity

--- src/main/java/com/Bigproject/ecommerce_springboot/entity/Cart.java
+++ src/main/java/com/Bigproject/ecommerce_springboot/entity/Cart.java
@@ -1,7 +1,6 @@
 package com.Bigproject.ecommerce_springboot.entity;
 
 
-import org.springframework.stereotype.Component;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,7 +9,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
-@Component
 @Entity
 public class Cart {
 
